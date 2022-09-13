b74809f0979bc81efca37784affbf4c3b189d50a
Kevin Gomes <kb.developer.br@gmail.com>
2022-09-13 14:15:12 -0400

    back-rate-limit init

---
+++ back-rate-limit/src/main/java/com/example/backratelimit/BackRateLimitApplication.java
@@ -0,0 +1,13 @@
+package com.example.backratelimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRateLimitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackRateLimitApplication.class, args);
+	}
+
+}
---
+++ back-rate-limit/src/test/java/com/example/backratelimit/BackRateLimitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backratelimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRateLimitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
