ffaf96c3120cf539c3cbad111a181bb3c16af619
Kevin Gomes <kb.developer.br@gmail.com>
2022-09-13 14:23:02 -0400

    back-rate-limit: endpoints

---
+++ back-rate-limit/src/main/java/com/example/backratelimit/controller/TestController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class TestController {
+
+    @Autowired
+    private BuildProperties buildProperties;
+
+    @PostMapping("/test")
+    public String testPost() {
+        return getUserLogged();
+    }
+
+    @PutMapping("/test")
+    public String testPut() {
+        return getUserLogged();
+    }
+
+    @DeleteMapping("/test")
+    public String testDelete() {
+        return getUserLogged();
+    }
+
+    @GetMapping("/test")
+    public String testGet() {
+        return getUserLogged();
+    }
+
+    private String getUserLogged() {
+        return "User logged: " + SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+    }
+
+}
